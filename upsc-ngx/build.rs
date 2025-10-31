c47c0c193f7b8c44ca8ba7363f63659d147fc923
luc-rodriguez <218833346+luc-rodriguez@users.noreply.github.com>
2025-10-31 12:49:55 -0500

    Added utilities, removed dx12 "support", improved build script

--- upsc-ngx/build.rs
+++ upsc-ngx/build.rs
@@ -2,15 +2,18 @@ use std::{env, path::PathBuf};
 // use dircpy::copy_dir;
 
 fn main() {
-    let dir = env!("CARGO_MANIFEST_DIR");
-    let sdk: String = format!("{}\\DLSS", dir);
-    let vkh: String = format!("{}\\..\\Vulkan-Headers", dir);
+    let cargo_manifest_dir = env!("CARGO_MANIFEST_DIR");
+
+    let sdk_dir = format!("{cargo_manifest_dir}\\DLSS");
+    let headers: String = format!("{sdk_dir}\\include");
+
+    let vulkan_headers = format!("{cargo_manifest_dir}\\..\\Vulkan-Headers\\include");
 
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 
     #[cfg(not(target_os = "windows"))]
     {
-        println!("cargo:rustc-link-search=native={sdk}/lib/Linux_x86_64");
+        println!("cargo:rustc-link-search=native={sdk_dir}/lib/Linux_x86_64");
         println!("cargo:rustc-link-lib=static=nvsdk_ngx");
         println!("cargo:rustc-link-lib=dylib=stdc++");
         println!("cargo:rustc-link-lib=dylib=dl");
@@ -18,45 +21,62 @@ fn main() {
 
     #[cfg(target_os = "windows")]
     {
-        println!("cargo:rustc-link-search=native={sdk}/lib/Windows_x86_64/x64");
+        println!("cargo:rustc-link-search=native={sdk_dir}/lib/Windows_x86_64/x64");
         #[cfg(not(target_feature = "crt-static"))]
         println!("cargo:rustc-link-lib=static=nvsdk_ngx_d");
         #[cfg(target_feature = "crt-static")]
         println!("cargo:rustc-link-lib=static=nvsdk_ngx_s");
     }
 
-    let mut builder = bindgen::Builder::default();
+    let mut builder = bindgen::Builder::default()
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .wrap_static_fns(true)
+        .wrap_static_fns_path(out_dir.join("wrap_static_fns"))
+        .clang_arg(format!("-I{headers}"))
+        .allowlist_item(".*NGX.*")
+        .blocklist_item(".*Cuda.*")
+        .blocklist_item(".*CUDA.*");
 
     #[cfg(feature = "vulkan")]
     {
         builder = builder
-            .header(format!("{}\\src\\vulkan.h", dir))
-            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-            .wrap_static_fns(true)
-            .wrap_static_fns_path(out_dir.join("wrap_static_fns"))
-            
-            .clang_arg(format!("-I{sdk}\\include"))
-            .allowlist_item(".*NGX.*")
-            .blocklist_item(".*Cuda.*")
-            .blocklist_item(".*CUDA.*")
-
-            .clang_arg(format!("-I{vkh}\\include"))
+            .headers([
+                format!("{vulkan_headers}\\vulkan\\vulkan.h"),
+                format!("{headers}\\nvsdk_ngx_helpers.h"),
+                format!("{headers}\\nvsdk_ngx_helpers_dlssd.h"),
+                format!("{headers}\\nvsdk_ngx_helpers_vk.h"),
+                format!("{headers}\\nvsdk_ngx_helpers_dlssd_vk.h"),
+            ])
+            .clang_arg(format!("-I{vulkan_headers}"))
             .blocklist_item("Vk.*")
             .blocklist_item("PFN_vk.*");
     }
 
+    /*
+    #[cfg(feature = "dx12")]
+    {
+        #include "nvsdk_ngx.h"
+        #include "nvsdk_ngx_defs.h"
+        #include "nvsdk_ngx_params.h"
+        #include "nvsdk_ngx_helpers.h"
+
+        builder = builder
+            .header(format!("{}\\src\\dx12.h", cargo_manifest_dir));
+    }
+    */
+
     builder
         .generate()
         .unwrap()
-        .write_to_file(out_dir.join("deps.rs"))
+        .write_to_file(out_dir.join("bindgen.rs"))
         .unwrap();
 
     cc::Build::new()
         .file(out_dir.join("wrap_static_fns.c"))
         .includes([
-            format!("{sdk}\\include"),
+            headers,
             #[cfg(feature = "vulkan")]
-            format!("{vkh}\\include"),
+            vulkan_headers,
         ])
         .compile("wrap_static_fns");
 }
--- upsc-ngx/src/lib.rs
+++ upsc-ngx/src/lib.rs
@@ -4,7 +4,7 @@
 #![allow(unused)]
 #![allow(unsafe_op_in_unsafe_fn)] // https://github.com/rust-lang/rust-bindgen/issues/3243
 
-include!(concat!(env!("OUT_DIR"), "/deps.rs"));
+include!(concat!(env!("OUT_DIR"), "/bindgen.rs"));
 
 #[cfg(feature = "vulkan")]
 use ash::vk::{
@@ -28,13 +28,23 @@ use ash::vk::{
     Image as VkImage,
 };
 
+use glam::{UVec2, Vec2};
 use wgpu::{Adapter, TextureUsages, TextureView};
 
 #[cfg(feature = "vulkan")]
 use wgpu::wgc::api::Vulkan;
 
+/*
+#[cfg(feature = "dx12")]
+use wgpu::wgc::api::Dx12;
+#[cfg(feature = "dx12")]
+use windows::Win32::{
+    Graphics::Direct3D12::*
+};
+*/
+
 #[cfg(feature = "vulkan")]
-pub fn texture_to_ngx(texture_view: &TextureView, adapter: &Adapter) -> NVSDK_NGX_Resource_VK {
+pub fn texture_to_ngx_vk(texture_view: &TextureView, adapter: &Adapter) -> NVSDK_NGX_Resource_VK {
     unsafe {
         let raw_view = texture_view.as_hal::<Vulkan>().unwrap().raw_handle();
         let texture = texture_view.texture();
@@ -64,6 +74,15 @@ pub fn texture_to_ngx(texture_view: &TextureView, adapter: &Adapter) -> NVSDK_NG
     }
 }
 
+//#[cfg(feature = "dx12")]
+//pub fn texture_to_ngx(texture_view: &TextureView, adapter: &Adapter) -> ID3D12Resource {
+    // NVSDK_NGX_D3D12_Feature_Eval_Params
+    // unsafe {
+    //    let raw_view = texture_view.as_hal::<Dx12>().unwrap();
+    //}
+//}
+
+/// https://en.wikipedia.org/wiki/Halton_sequence
 pub fn halton_sequence(mut index: u32, base: u32) -> f32 {
     let mut f = 1.0;
     let mut result = 0.0;
@@ -75,4 +94,19 @@ pub fn halton_sequence(mut index: u32, base: u32) -> f32 {
     }
 
     result
+}
+
+pub fn mip_bias(source: UVec2, target: UVec2) -> f32 {
+    (source.x as f32 / target.x as f32).log2() - 1.0
+}
+
+pub fn sub_pixel_jitter(source: UVec2, target: UVec2, frame_number: u32) -> Vec2 {
+    let ratio = target.x as f32 / source.x as f32;
+    let phase_count = (8.0 * ratio * ratio) as u32;
+    let i = frame_number % phase_count;
+
+    Vec2 {
+        x: halton_sequence(i, 2),
+        y: halton_sequence(i, 3),
+    } - 0.5
 }
