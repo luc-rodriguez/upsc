158b5c120a50d04209b2eb7164a9606130ba4ddc
luc-rodriguez <218833346+luc-rodriguez@users.noreply.github.com>
2025-10-31 14:28:57 -0500

    Fixed dependency resolution

---
+++ upsc-fidelityfx/src/lib.rs
@@ -0,0 +1,7 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unused)]
+#![allow(unsafe_op_in_unsafe_fn)] // https://github.com/rust-lang/rust-bindgen/issues/3243
+
+include!(concat!(env!("OUT_DIR"), "/bindgen.rs"));
