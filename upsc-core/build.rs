3120e9acdf851878c77762b07d4d9530f49b842a
luc-rodriguez <218833346+luc-rodriguez@users.noreply.github.com>
2025-11-03 17:15:14 -0600

    Improvements to the `dlss` flag

--- upsc-core/build.rs
+++ upsc-core/build.rs
@@ -2,6 +2,11 @@ use cfg_aliases::cfg_aliases;
 
 fn main() {
     cfg_aliases! {
-        dlss: { any(windows, target_os = "linux") }
+        dlss: {
+            all(
+                any(target_os = "windows", target_os = "linux"),
+                any(target_arch = "x86_64", target_arch = "aarch64")
+            )
+        }
     }
 }
