b6fc217c844f4e883f04348383397c4edb1038d3
luc-rodriguez <218833346+luc-rodriguez@users.noreply.github.com>
2025-11-03 17:38:21 -0600

    Added new cfg aliases

--- upsc-core/build.rs
+++ upsc-core/build.rs
@@ -2,7 +2,19 @@ use cfg_aliases::cfg_aliases;
 
 fn main() {
     cfg_aliases! {
-        dlss: {
+        ngx: {
+            all(
+                any(target_os = "windows", target_os = "linux"),
+                any(target_arch = "x86_64", target_arch = "aarch64")
+            )
+        },
+        ffx: {
+            all(
+                target_arch = "x86_64",
+                any(target_os = "windows", target_os = "linux")
+            )
+        },
+        xess: {
             all(
                 any(target_os = "windows", target_os = "linux"),
                 any(target_arch = "x86_64", target_arch = "aarch64")
